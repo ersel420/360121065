4107a72142d0a96b543ae57f7bd6eafae916fef7
Ersel <30392208+ersel420@users.noreply.github.com>
2023-01-02 15:39:12 +0300

    Update views.py

--- payment/views.py
+++ payment/views.py
@@ -2,13 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 from cart.cart import Cart
 from cart.models import CartItem
-import stripe
+from .models import Order, OrderItem
 import iyzipay
 import json
-from django.http import HttpResponse
-from .models import Order, OrderItem
 
 options = {
     'api_key': 'sandbox-pgj9MQ5ZJVltOtVbCuPjwqy2OCcBmjku',
