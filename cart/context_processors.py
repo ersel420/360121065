951e96c60a42eded31ef10ed6d77f0b54efe7f16
Ersel <30392208+ersel420@users.noreply.github.com>
2023-01-02 15:41:57 +0300

    Update context_processors.py

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -3,9 +3,3 @@ from .models import CartItem
 
 def cart(request): #Cart Items
     return {'cart': Cart(request)}
-
-# def cartQty(request): #Calculate Cart Qty
-#     qty = 0
-#     for item in CartItem.objects.filter(user = request.user):
-#         qty += item.itemQty
-#     return {'qty': qty}
